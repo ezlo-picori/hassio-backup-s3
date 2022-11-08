406b95b279210fe2ee2e0ff18948d9c6a7056892
Ezlo Picori <ezlo@protonmail.com>
2022-11-08 23:17:55 +0100

    Add endpoint url option

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 2.3.0
+
+- Add option to define S3 endpoint URL
+
 ## 2.2.3
 
 - Fix url to readme
--- backup-s3/run.sh
+++ backup-s3/run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+ENDPOINT=`jq -r .awsendpoint /data/options.json`
 KEY=`jq -r .awskey /data/options.json`
 SECRET=`jq -r .awssecret /data/options.json`
 BUCKET=`jq -r .bucketname /data/options.json`
@@ -8,9 +9,9 @@ now="$(date +'%d/%m/%Y - %H:%M:%S')"
 
 echo $now
 
-aws configure set aws_access_key_id $KEY
-aws configure set aws_secret_access_key $SECRET
+aws configure set aws_access_key_id ${KEY}
+aws configure set aws_secret_access_key ${SECRET}
 
-aws s3 sync /backup/ s3://$BUCKET/  --delete
+aws s3 sync /backup/ s3://$BUCKET/  --delete --endpoint-url ${ENDPOINT}
 
 echo "Done"
